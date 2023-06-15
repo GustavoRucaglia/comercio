5a11137aa21e151150cc807368503b0bf87a548b
Gustavo Santiago <gustavorucaglia@gmail.com>
2023-06-15 08:17:09 -0300

    initial commit

---
+++ src/main/java/com/itb/comercio/model/Produto.java
@@ -0,0 +1,36 @@
+package com.itb.comercio.model;
+
+public class Produto {
+	private Long id;
+	private String name;
+	private String descreption;
+	private String codeBar;
+	private double preco;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getDescreption() {
+		return descreption;
+	}
+	public void setDescreption(String descreption) {
+		this.descreption = descreption;
+	}
+	public String getCodeBar() {
+		return codeBar;
+	}
+	public void setCodeBar(String codeBar) {
+		this.codeBar = codeBar;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	
+	
+
+}
